8da13355e3336e2a36a440d4defc2d106013b5c3
nutr1t07 <nutr1t07@outlook.com>
2020-03-13 20:24:04 +0800

    [refactor] Timer.hs shorten

--- src/Plugin/Timer.hs
+++ src/Plugin/Timer.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Plugin.Timer where
 
-import Core.Web.CoolQ               as CQ
-import Core.Data.CoolQ               as CQ
-import Core.Type.CoolQ.Update               as CQ
+import           Core.Web.CoolQ                as CQ
+import           Core.Data.CoolQ               as CQ
+import           Core.Type.CoolQ.Update        as CQ
+import           Utils.Config
+import           Utils.Logging
 
-import Utils.Config
-import Utils.Logging
-
-import Data.Time
-import Control.Concurrent
-import qualified Data.Text as Text
-import Data.Text.Read as Text
-import Data.Text.IO as Text
-import Data.Either
-import Data.Maybe
+import           Data.Time
+import           Control.Concurrent
+import qualified Data.Text                     as Text
+import           Data.Text.Read                as Text
+import           Data.Text.IO                  as Text
+import           Data.Either
+import           Data.Maybe
+import           Data.List
 
 timeFormat :: String
 timeFormat = "%Y/%m/%d-%H:%M"
@@ -39,54 +39,54 @@ checkTimer config = do
   _ <- traverse (\userId -> sendPrivMsg userId "Time's up." config) users
   threadDelay 60000000 >> checkTimer config
 
+rmTimeOutUser :: IO [Integer]
+rmTimeOutUser = do
+  fileContent <- Text.readFile "timers.txt"
+  let timers = map (Text.breakOn " ") (init $ Text.splitOn "\n" fileContent)
+  timerList <- traverse checkIfTimesOut timers
+  _ <- Text.writeFile "timers.txt" $ (mconcat.intersperse "\n") (lefts timerList)
+  pure $ rights timerList
+  where
+    checkIfTimesOut ("",_) = pure $ Left ""
+    checkIfTimesOut (time, userId) = do
+      nowTime <- getCurrentTime
+      if nowTime > readCusTime (Text.unpack time)
+         then pure $ Right $ fromRight 1 $ fst <$> decimal (Text.drop 1 userId)
+         else pure $ Left (time<>userId)
+
 processTimerOp :: Config -> CQ.Update -> IO (Maybe RespBS)
 processTimerOp config cqUpdate =
   let msgTxt = getText (CQ.message cqUpdate)
       userId = CQ.user_id cqUpdate           in
-    case Text.take 3 msgTxt of
-      "/al" ->
-        let content = Text.dropWhile (==' ') (Text.drop 3 msgTxt) in
-          if content /= "" && isRight (decimal content)
-             then do
-               timers <- Text.readFile "timers.txt"
-               let alarmTime = 60 * fromIntegral (fromRight 0 $ fst <$> decimal content::Int)
-               timerState <- addTimer alarmTime (fromJust userId)
-               case timerState of
-                 Just _ -> logWT Info ("Pomodoro set from " <> show userId) >> Just <$> sendBackTextMsg (Text.pack $ show alarmTime <> " alarm is set up.") cqUpdate config
-                 _      -> Just <$> sendBackTextMsg "You're having an on-going task." cqUpdate config
-               logWT Info ("Alarm [" <> show alarmTime <>"] set from " <> show userId)
-               Just <$> sendBackTextMsg "The alarm time is up." cqUpdate config
-             else Just <$> sendBackTextMsg "Bad format.\nUsage: /al TIME\nTIME uses the unit of minutes." cqUpdate config
-      "/tm" -> do
-        timers <- Text.readFile "timers.txt"
-        timerState <- addTimer 60 (fromJust userId)
-        case timerState of
-          Just _ -> logWT Info ("Pomodoro set from " <> show userId) >> Just <$> sendBackTextMsg "Pomodoro 25 minutes started." cqUpdate config
-          _      -> Just <$> sendBackTextMsg "You're having an on-going task." cqUpdate config
+  case Text.take 3 msgTxt of
+    "/al" ->
+      if content /= "" && isRight (decimal content)
+        then do
+          let alarmTime = 60 * fromIntegral (fromRight 0 $ fst <$> decimal content::Int)
+          timerState <- addTimer alarmTime (fromJust userId)
+          case timerState of
+            Just _ -> logWT Info ("Alarm ["<>show alarmTime<>"] set from " <> show userId) >>
+              Just <$> sendBackTextMsg (Text.pack$show alarmTime<>" alarm is set up.") cqUpdate config
+            _      -> Just <$> sendBackTextMsg "You're having an on-going task." cqUpdate config
+        else Just <$> sendBackTextMsg
+          "Bad format.\nUsage: /al TIME\nTIME uses the unit of minutes." cqUpdate config
+      where
+        content = Text.dropWhile (==' ') (Text.drop 3 msgTxt)
 
-      "/cl" -> do
-        fileContent <- Text.readFile "timers.txt"
-        let timers = Prelude.init $ Text.splitOn "\n" fileContent
-        let afterRmTimers =
-              filter (\timer -> snd (Text.breakOn (Text.pack $show$ fromJust userId) timer) == "") timers
-        _ <- Text.writeFile "timers.txt" $ foldr (\a b -> a <> "\n" <> b) "" afterRmTimers
-        Just <$> sendBackTextMsg "Alarm cancelled." cqUpdate config
-      _ -> pure Nothing
+    "/pd" -> do
+      timerState <- addTimer (60*25) (fromJust userId)
+      case timerState of
+        Just _ -> logWT Info ("Pomodoro set from " <> show userId) >>
+                  Just <$> sendBackTextMsg "Pomodoro 25 minutes started." cqUpdate config
+        _      -> Just <$> sendBackTextMsg "You're having an on-going task." cqUpdate config
 
-rmTimeOutUser :: IO [Integer]
-rmTimeOutUser = do
-  fileContent <- Text.readFile "timers.txt"
-  let timers = fmap (Text.breakOn " ") (Prelude.init $ Text.splitOn "\n" fileContent)
-  outUsers <- traverse checkIfTimesOut timers
-  let timeOutUsers = Text.pack.(" "++).show.fromJust <$> filter isJust outUsers
-  let notTimeOutUsers = filter (\(_,userId) -> userId `notElem` timeOutUsers) timers
-  _ <- Text.writeFile "timers.txt" $ foldr (\a b -> a <> "\n" <> b) "" (fmap mergeTuple notTimeOutUsers)
-  pure $ fromJust $ sequence $ filter isJust outUsers
-  where
-    mergeTuple (a,b) = a <> b
-    checkIfTimesOut ("",_) = pure Nothing
-    checkIfTimesOut (time, userId) = do
-      nowTime <- getCurrentTime
-      if nowTime > readCusTime (Text.unpack time)
-         then pure $ Just <$> fromRight 1 $ fst <$> decimal (Text.drop 1 userId)
-         else pure Nothing
+    "/cl" -> do
+      fileContent <- Text.readFile "timers.txt"
+      let timers = Prelude.init $ Text.splitOn "\n" fileContent
+      let afterRmTimers =
+            filter (\timer -> snd (Text.breakOn (Text.pack $show$ fromJust userId) timer) == "") timers
+      _ <- Text.writeFile "timers.txt" $ foldr (\a b -> a <> "\n" <> b) "" afterRmTimers
+      Just <$> sendBackTextMsg "Alarm cancelled." cqUpdate config
+
+    _ -> pure Nothing
+
