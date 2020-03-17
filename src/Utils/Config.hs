d75a3cf104102ae8e32e66fc63f9e74c07badf94
nutr1t07 <nutr1t07@outlook.com>
2020-03-17 13:47:12 +0800

    [fix] Config fix

--- src/Utils/Config.hs
+++ src/Utils/Config.hs
@@ -4,31 +4,12 @@ module Utils.Config where
 import Data.Aeson
 import GHC.Generics
 
-type GroupMap = [(Integer, Integer)]
-
 data Config = Config {
     admins     :: [Integer]
   , port       :: Int
   , thisServer :: String
   , cqServer   :: String
   , tgbotToken :: String
-  , groups     :: GroupMap
-  , forwardOn  :: Bool
-  , searchOn   :: Bool
 } deriving (Eq, Show, Generic)
 
 instance FromJSON Config
-instance ToJSON Config
-
--- exampleConfig = Config
---                   [-14143424243]
---                   8443
---                   "http://localhost:5700"
---                   "telegramtokenhere"
---                   [(12312313, - 45412346)]
--- 
--- makeExampleConfig :: SomeException -> IO ByteString
--- makeExampleConfig _ = do
---   logWT "Error" "No config file found. Creating example file."
---   BL.writeFile "config.json" (encode exampleConfig)
---   return (encode exampleConfig)
